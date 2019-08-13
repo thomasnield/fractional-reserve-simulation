834692679c7fc50ebd6e3825c0474a471d19eb4f
Thomas Nield <thomasnield@live.com>
2019-08-13 18:42:11 -0500

    initial commit

--- src/main/kotlin/Launcher.kt
+++ src/main/kotlin/Launcher.kt
@@ -1,3 +1,7 @@
+
+// look up "modern money mechanics"
+// Biased video: https://www.youtube.com/watch?v=P-5xDzTvW6E
+
 fun main() {
 	println("Hello")
 }
@@ -21,6 +25,13 @@ class FederalGovernment: Entity() {
 	val receivedLoans = mutableListOf<Loan>()
 }
 
-class Loan(val interestRate: Double, val principal: Double) {
-	var currentBalance = principal
+sealed class Account {
+	abstract val amount: Double
 }
+
+class Loan(val interestRate: Double, val principal: Double): Account() {
+	override var amount = principal
+}
+
+class Deposit(val interestRate: Double,
+			  override val amount: Double): Account()
