67f0f9a5f7d4dce98330e2507ab85838cc97dc69
Thomas Nield <thomasnield@live.com>
2019-08-13 18:37:37 -0500

    initial commit

--- src/main/kotlin/Launcher.kt
+++ src/main/kotlin/Launcher.kt
@@ -1,3 +1,26 @@
 fun main() {
 	println("Hello")
 }
+
+
+sealed class Entity
+
+class Consumer: Entity() {
+	val receivedLoans = mutableListOf<Loan>()
+}
+
+class Bank: Entity() {
+	val sentLoans = mutableListOf<Loan>()
+}
+
+class FederalReserve: Entity() {
+	val sentLoans = mutableListOf<Loan>()
+}
+
+class FederalGovernment: Entity() {
+	val receivedLoans = mutableListOf<Loan>()
+}
+
+class Loan(val interestRate: Double, val principal: Double) {
+	var currentBalance = principal
+}
